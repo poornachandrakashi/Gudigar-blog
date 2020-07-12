31a587ffab9613cea1cdcd4d4684ef53bc30e7d4
poornachandra kashi <44542685+poornachandrakashi@users.noreply.github.com>
2020-07-12 19:23:02 +0530

    bfjdbj

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask,render_template,flash,url_for,redirect,session,logging
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib import sha256_crypt
+
+app=Flask(__name__)
+
+Articles=Articles()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/articles')
+def articles():
+    return render_template('articles.html',articles=Articles)
+    
+@app.route('/article/<string:id>')
+def article(id):
+    return render_template('article.html',id=id)
+
+class RegisterForm(Form):
+    name= StringField('Name',[validators.Length(min=1,max=50)])
+    username=StringField('Username',[validators.length(min=5,max=25)])
+    email=StringField('Email',[validators.length(min=6,max=50)])
+    password=PasswordField('Password',[
+        validators.DataRequired(),
+        validators.EqualTo('confirm',messege="Password do nt match")
+    ])
+    confirm=PasswordField('Confirm Password')
+    
+@app.route('/register' ,methods=['GET','POST'])
+def register():
+    form=RegisterForm()
+    if request.method=='POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html',form=form)
+
+if __name__=='__main__':
+    app.run(debug=True)
